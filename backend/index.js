8d707a5c0b3da63cd62181a28ab38ed9a7688377
Joakim Teixeira <setokim1@hotmail.com>
2021-02-01 09:16:48 -0300

    Add backend initial files

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+// Import libraries
+const express = require('express')
+const cors = require('cors')
+
+// Setup express
+const app = express()
+
+// Setup middleware
+app.use(express.json())
+app.use(cors())
+
+// Setup port
+const PORT = process.env.PORT || 3001
+
+// Start server
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`))
