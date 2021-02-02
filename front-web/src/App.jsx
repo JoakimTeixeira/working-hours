559a3a43aa93a01cf82c268a2fe5701dce9c7d51
Joakim Teixeira <setokim1@hotmail.com>
2021-02-01 22:40:25 -0300

    Add input component

--- front-web/src/App.jsx
+++ front-web/src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Input from 'components/Input';
 
 function App() {
-  return <h1>Working hours</h1>;
+  return (
+    <>
+      <h1>Working hours</h1>
+      <Input />
+      <Input />
+    </>
+  );
 }
 
 export default App;
---
+++ front-web/src/components/Input.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Input = () => {
+  return <input type="time" name="input" />;
+};
+
+export default Input;
