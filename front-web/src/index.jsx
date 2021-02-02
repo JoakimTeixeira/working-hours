c5bf5d40680cba9c872b0580c5cd20df353e5a7a
Joakim Teixeira <setokim1@hotmail.com>
2021-02-01 22:40:09 -0300

    Add materialize configuration

--- front-web/src/index.jsx
+++ front-web/src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../node_modules/materialize-css/dist/js/materialize.min';
 
 ReactDOM.render(
   <React.StrictMode>
