a4a8e101a82cf44903a914650476645d813bc66f
Joakim Teixeira <setokim1@hotmail.com>
2021-02-09 20:13:24 -0300

    Move functions inside the proper components

--- front-web/src/components/Button.jsx
+++ front-web/src/components/Button.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Axios from 'axios';
 
-const Button = ({ convertTimeToMinutes }) => {
+const Button = ({ time, setTime }) => {
   Button.propTypes = {
-    convertTimeToMinutes: PropTypes.func.isRequired,
+    time: PropTypes.shape({
+      t1: PropTypes.string,
+      t2: PropTypes.string,
+    }).isRequired,
+    setTime: PropTypes.func.isRequired,
+  };
+
+  const convertTimeToMinutes = async () => {
+    const timeInMinutes = await Axios.post(
+      'http://localhost:3001/convert/toMinutes',
+      { t1: time.t1, t2: time.t2 },
+      {
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+    setTime(timeInMinutes.data);
   };
 
   return (
--- front-web/src/components/Input.jsx
+++ front-web/src/components/Input.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Input = ({ inputName, handleInput }) => {
+const Input = ({ inputName, time, setTime }) => {
   Input.propTypes = {
     inputName: PropTypes.string.isRequired,
-    handleInput: PropTypes.func.isRequired,
+    time: PropTypes.shape({
+      t1: PropTypes.string,
+      t2: PropTypes.string,
+    }).isRequired,
+    setTime: PropTypes.func.isRequired,
+  };
+
+  const handleInput = (e) => {
+    if (inputName === 't1') setTime({ ...time, t1: e.target.value });
+    if (inputName === 't2') setTime({ ...time, t2: e.target.value });
   };
 
   return <input type="time" name={inputName} onChange={(e) => handleInput(e, inputName)} />;
